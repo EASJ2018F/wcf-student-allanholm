edefd48ffcb47e73bebb67d24996af3f9c662de3
Allan Holm Søndergaard <alla2653@edu.easj.dk>
2018-02-12 16:25:25 +0100

    student klasse oprettet og udfyldt

---
+++ WcfService1allan/WcfService1allan/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace WcfService1allan
+{
+    public class Student
+    {
+        private string _name;
+        private string _className;
+        private int _studentID;
+
+        public Student(string name, string className, int studentID)
+        {
+            _name = name;
+            _className = className;
+            _studentID = studentID;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string ClassName
+        {
+            get { return _className; }
+        }
+
+        public int StudentID
+        {
+            get { return _studentID; }
+        }
+    }
+}
